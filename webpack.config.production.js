f9cac203aa7e4f55041d8f6a80e12e42d64c7039
webzuu <info@webzuu.pl>
2017-06-07 15:29:08 +0200

    Initialize from react-hot-boilerplate

---
+++ webpack.config.production.js
@@ -0,0 +1,35 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  entry: './src/index.js',
+
+  output: {
+    filename: 'static/bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/'
+  },
+
+  devtool: 'source-map',
+
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+
+  module: {
+    rules: [{
+      test: /\.jsx?$/,
+      use: [
+        'babel-loader'
+      ],
+      exclude: /node_modules/
+    }]
+  },
+
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      sourceMap: true,
+      comments: false
+    })
+  ]
+};
