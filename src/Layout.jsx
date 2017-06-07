a794d8ee7e8f24c4076df7731a594c8d7703e0f8
webzuu <info@webzuu.pl>
2017-06-07 15:43:58 +0200

    Setup material-ui

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
+
 export default function Layout({ children }) {
   return (
-    <div>
-      <h1>Hello, world!</h1>
-      {children}
-    </div>
+    <MuiThemeProvider>
+      <div>
+        <h1>Hello, world!</h1>
+        {children}
+      </div>
+    </MuiThemeProvider>
   );
 }
